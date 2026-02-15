dccf1a60da7a8805c30ccc368d89da6a6fc5e107
AdrianRiveroll <adrianriceeoll981@gmail.com>
2026-02-15 02:26:32 -0600

    Se subio la base de datos

---
+++ inventario_db.sql
@@ -0,0 +1,200 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-02-2026 a las 09:25:52
+-- Versión del servidor: 8.4.0
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `storage_capacity` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stock` int NOT NULL,
+  `price` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `brand`, `storage_capacity`, `stock`, `price`) VALUES
+(1, 'Teclado mecánico', 'Redragon', 'N/A', 22, 699.00),
+(2, 'Mouse gamer', 'Logitech', 'N/A', 25, 499.00),
+(3, 'USB 3.0', 'Kingston', '64GB', 39, 169.00),
+(4, 'SSD', 'Crucial', '500GB', 10, 999.00),
+(5, 'Audífonos', 'HyperX', 'N/A', 14, 899.00),
+(6, 'Silla Gamer', 'MSI', 'N/A', 15, 1299.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` bigint NOT NULL,
+  `created_at` datetime NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sales`
+--
+
+INSERT INTO `sales` (`id`, `created_at`, `customer_name`, `total`) VALUES
+(1, '2026-02-13 20:10:40', 'Cliente Numero 1', 1198.00),
+(2, '2026-02-15 04:25:57', 'cliente', 1198.00),
+(3, '2026-02-15 05:25:11', 'cliente', 8159.00),
+(4, '2026-02-15 05:49:05', 'cliente', 998.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale_items`
+--
+
+CREATE TABLE `sale_items` (
+  `id` bigint NOT NULL,
+  `sale_id` bigint NOT NULL,
+  `product_id` bigint NOT NULL,
+  `quantity` int NOT NULL,
+  `unit_price` decimal(12,2) NOT NULL,
+  `line_total` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sale_items`
+--
+
+INSERT INTO `sale_items` (`id`, `sale_id`, `product_id`, `quantity`, `unit_price`, `line_total`) VALUES
+(1, 1, 2, 1, 499.00, 499.00),
+(2, 1, 1, 1, 699.00, 699.00),
+(3, 2, 1, 1, 699.00, 699.00),
+(4, 2, 2, 1, 499.00, 499.00),
+(5, 3, 5, 4, 899.00, 3596.00),
+(6, 3, 4, 2, 999.00, 1998.00),
+(7, 3, 3, 1, 169.00, 169.00),
+(8, 3, 2, 2, 499.00, 998.00),
+(9, 3, 1, 2, 699.00, 1398.00),
+(10, 4, 2, 2, 499.00, 998.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password_hash`, `role`) VALUES
+(1, 'admin', '$2a$10$.Dy53l2yvJoB0JQ50Y4JreNv7Bp5RPtLUrNVi6d/P3kPur5WUXfjK', 'ADMIN'),
+(2, 'cliente', '$2a$10$KEd7t8aH1fqbJ4fST3Y3n.tzBVBTuPfHzsWU/HepTfmsnZ6UfFrIu', 'CLIENT'),
+(3, 'client', '$2a$10$lJe.ScXNbqTOaWWIYkSn0eHgqiZYItSQgbQRWCtKvLOIKr9.l6Gmm', 'CLIENT');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sale_items`
+--
+ALTER TABLE `sale_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_sale_items_sale` (`sale_id`),
+  ADD KEY `fk_sale_items_product` (`product_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `sale_items`
+--
+ALTER TABLE `sale_items`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `sale_items`
+--
+ALTER TABLE `sale_items`
+  ADD CONSTRAINT `fk_sale_items_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_sale_items_sale` FOREIGN KEY (`sale_id`) REFERENCES `sales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
